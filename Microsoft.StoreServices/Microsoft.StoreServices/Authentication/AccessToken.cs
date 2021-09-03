327ed2f4565740bdc8c1cd21d11db3fda70219c7
Cameron Goodwin <Cameron.Goodwin@microsoft.com>
2021-09-03 10:20:49 -0700

    Fixing file headers to reference the MIT liciense. (#9)
    * Fixing file headers to reference the MIT liciense.
    
    * removing added whitespace.
    
    Co-authored-by: Cameron Goodwin <cagood@microsoft.com>

--- Microsoft.StoreServices/Microsoft.StoreServices/Authentication/AccessToken.cs
+++ Microsoft.StoreServices/Microsoft.StoreServices/Authentication/AccessToken.cs
@@ -2,7 +2,9 @@
 // AccessToken.cs
 //
 // Xbox Advanced Technology Group (ATG)
-// Copyright (C) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License. See License file under the project root for
+// license information.
 //-----------------------------------------------------------------------------
 
 using Newtonsoft.Json;
